4a78bd386f43864bfc48645f5a1f50d49336d0b1
Daria Schek <sdasha81@yandex.ru>
2019-02-21 17:50:10 +0300

    Git cleanup

--- shop/src/main/java/ru/ncedu/schek/shop/repos/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ncedu.schek.shop.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ru.ncedu.schek.shop.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-	User findByUsername(String username);
-}
