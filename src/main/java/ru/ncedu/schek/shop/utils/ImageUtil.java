c6b2574338f148f4396b7f7f7255041881e38b95
Daria Schek <sdasha81@yandex.ru>
2019-02-25 01:45:19 +0300

    Solved jar picture problem

--- src/main/java/ru/ncedu/schek/shop/utils/ImageUtil.java
+++ src/main/java/ru/ncedu/schek/shop/utils/ImageUtil.java
@@ -1,14 +1,11 @@
 package ru.ncedu.schek.shop.utils;
 
 import java.io.*;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.spi.FileSystemProvider;
-import java.util.Collections;
+
+import org.apache.commons.io.IOUtils;
 
 import lombok.experimental.UtilityClass;
 
@@ -17,15 +14,11 @@ public class ImageUtil {
 
     public static byte[] loadImage(String filePath) {
         try {
-          //  return Files.readAllBytes(Paths.get(ImageUtil.class.getResource(filePath).toURI()));
-
-            URI uri = ImageUtil.class.getResource(filePath).toURI();
-            
-            Path source = Paths.get(uri);
+           // return Files.readAllBytes(Paths.get(ImageUtil.class.getResource(filePath).toURI()));
+        	return IOUtils.toByteArray(ImageUtil.class.getResourceAsStream(filePath));
            
-            return Files.readAllBytes(source);
             
-        } catch (IOException | URISyntaxException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
