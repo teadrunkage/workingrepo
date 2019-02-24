33b80accf297d8659af922d80fa470e32a692ed2
Daria Schek <sdasha81@yandex.ru>
2019-02-24 21:30:51 +0300

    Latest commit

--- src/main/java/ru/ncedu/schek/shop/utils/ImageUtil.java
+++ src/main/java/ru/ncedu/schek/shop/utils/ImageUtil.java
@@ -1,9 +1,15 @@
 package ru.ncedu.schek.shop.utils;
 
 import java.io.*;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.Collections;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -11,7 +17,14 @@ public class ImageUtil {
 
     public static byte[] loadImage(String filePath) {
         try {
-            return Files.readAllBytes(Paths.get(ImageUtil.class.getResource(filePath).toURI()));
+          //  return Files.readAllBytes(Paths.get(ImageUtil.class.getResource(filePath).toURI()));
+
+            URI uri = ImageUtil.class.getResource(filePath).toURI();
+            
+            Path source = Paths.get(uri);
+           
+            return Files.readAllBytes(source);
+            
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
